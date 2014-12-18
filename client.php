406c7b5ae9c518d9b69dc7c7d5141557e2faf74e
sedacovem <m.sedacove@gmail.com>
2014-12-18 17:05:56 +0400

    some files

---
+++ client.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Way Cup Coffee</title>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="css/style.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+   <div class="container header-crm">
+     <div class="row">
+      <div class="span4"><a href="index.html" class="logo-head"></a></div>
+      <div class="span4">
+        <div class="time">
+          <h2>10 июля</h2>
+          <span>10:42</span>
+        </div>
+      </div>
+      <div class="span4">
+        <div class="login-out">
+          Way-cup-admin<br>
+          <a href="#">Выйти</a>
+        </div>
+      </div>
+    </div>
+   </div>
+
+   <div class="container main-block">
+     <div class="row">
+       <div class="span2">
+         <div class="menu-left">
+          <ul>
+            <li><a href="index-crm.php"><i class="icon-th-list"></i> Дашбоард</a></li>
+            <li><a href="client.php" id="active"><i class="icon-user"></i> Клиенты</a></li>
+            <li><a href="menu.php"><i class="icon-list-alt"></i> Меню</a></li>
+            <li><a href="warehouse.php"><i class="icon-home"></i> Склад</a></li>
+            <li><a href="sale.php"><i class="icon-shopping-cart"></i> Продажи</a></li>
+            <li><a href="admin-back.php"><i class="icon-wrench"></i> Админка</a></li>
+          </ul>
+         </div>
+       </div>
+       <div class="span10">
+         <div class="main-content">
+          <div class="table-client">
+           <table>
+             <thead>
+               <td>Номер карты</td>
+               <td>ФИО</td>
+               <td>Телефон</td>
+               <td>Бонусы</td>
+             </thead>
+             <tbody>
+              <tr>
+                <td>0001</td>
+                <td>Седаков Михаил Сергеевич</td>
+                <td>8 919 899 82 38</td>
+                <td>
+                  <i></i>
+                  <i></i>
+                  <i></i>
+                  <i></i>
+                  <i></i>
+                </td>
+              </tr>
+                <tr>
+                <td>0001</td>
+                <td>Седаков Михаил Сергеевич</td>
+                <td>8 919 899 82 38</td>
+                <td>4</td>
+              </tr>
+               <tr>
+                <td>0001</td>
+                <td>Седаков Михаил Сергеевич</td>
+                <td>8 919 899 82 38</td>
+                <td> <i></i>
+                  <i></i>
+                  <i></i></td>
+              </tr>
+               <tr>
+                <td>0001</td>
+                <td>Седаков Михаил Сергеевич</td>
+                <td>8 919 899 82 38</td>
+                <td>4</td>
+              </tr>
+               <tr>
+                <td>0001</td>
+                <td>Седаков Михаил Сергеевич</td>
+                <td>8 919 899 82 38</td>
+                <td>4</td>
+              </tr>
+               <tr>
+                <td>0001</td>
+                <td>Седаков Михаил Сергеевич</td>
+                <td>8 919 899 82 38</td>
+                <td>4</td>
+              </tr>
+               <tr>
+                <td>0001</td>
+                <td>Седаков Михаил Сергеевич</td>
+                <td>8 919 899 82 38</td>
+                <td>4</td>
+              </tr>
+             </tbody>
+           </table>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+  </body>
+</html>
